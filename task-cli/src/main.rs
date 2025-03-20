3856c988be54a1918f42ad003636b86d1c43d6c6
tacascer <trandangtrithanh2000@gmail.com>
2025-03-20 00:02:53 -0400

    feat(workspace): remove unused crates and workspace members
    Removed `todo-graph`, `todo-graph-server`, and `todo-list` modules along with associated dependencies from the workspace. This cleanup reduces complexity and removes unneeded code and dependencies, streamlining the project structure.
    ```

--- task-cli/src/main.rs
+++ task-cli/src/main.rs
@@ -1,3 +1,5 @@
+mod task;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
--- task-cli/src/task.rs
+++ task-cli/src/task.rs
@@ -1,4 +1,17 @@
-use crate::Task;
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+struct Task {
+    id: u32,
+    description: String,
+    status: Status,
+    created_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+enum Status {
+    #[default]
+    Todo,
+}
 
 struct TaskRepository {
     tasks: Vec<Task>,
