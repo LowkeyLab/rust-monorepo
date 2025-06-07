31e4960df45a2a6bd11db7b24cbd7578e700bd05
tacascer <trandangtrithanh2000@gmail.com>
2025-06-08 00:41:54 +0100

    feat(nicknamer): add `Nicknamer` trait and implementation
    Introduced the `Nicknamer` trait and its implementation using `NicknamerImpl`. This includes a `reveal_all` method to utilize the `Revealer` behavior and handle name revealing asynchronously.

--- nicknamer/src/nicknamer/mod.rs
+++ nicknamer/src/nicknamer/mod.rs
@@ -2,3 +2,35 @@ pub(crate) mod commands;
 mod config;
 
 pub(crate) mod connectors;
+
+use commands::Error;
+use commands::names::NamesRepository;
+use commands::reveal::{Revealer, RevealerImpl};
+use connectors::discord::DiscordConnector;
+
+trait Nicknamer {
+    async fn reveal_all(&self) -> Result<(), Error>;
+}
+
+struct NicknamerImpl<'a, REPO: NamesRepository, DISCORD: DiscordConnector> {
+    names_repository: &'a REPO,
+    discord_connector: &'a DISCORD,
+}
+
+impl<'a, REPO: NamesRepository, DISCORD: DiscordConnector> NicknamerImpl<'a, REPO, DISCORD> {
+    fn new(names_repository: &'a REPO, discord_connector: &'a DISCORD) -> Self {
+        Self {
+            names_repository,
+            discord_connector,
+        }
+    }
+}
+
+impl<'a, REPO: NamesRepository, DISCORD: DiscordConnector> Nicknamer
+    for NicknamerImpl<'a, REPO, DISCORD>
+{
+    async fn reveal_all(&self) -> Result<(), Error> {
+        let revealer = RevealerImpl::new(self.names_repository, self.discord_connector);
+        revealer.reveal_all().await
+    }
+}
