0a86f5e3fb299406e26ee6b7350fd8790d2acb94
tacascer <trandangtrithanh2000@gmail.com>
2025-06-08 23:27:55 +0100

    feat: add config support for nicknamer
    Introduced a configuration file and struct parsing for `nicknamer`. This allows easier customization of reveal settings like insults and roles to mention.

--- nicknamer/src/nicknamer/config.rs
+++ nicknamer/src/nicknamer/config.rs
@@ -1,16 +1,30 @@
+use serde::Deserialize;
+
 pub const REVEAL_INSULT: &str = "ya dingus";
 pub const CODE_MONKEYS_ROLE_NAME: &str = "Code Monkeys";
-#[allow(dead_code)]
-pub const URL_LENGTH_VIOLATION_FACTOR: u32 = 2;
-#[allow(dead_code)]
-pub const JAR_JAR_EMOJI_ID: u64 = 1061775549065855079;
-#[allow(dead_code)]
-pub const ZACH_USER_ID: u64 = 894692357457469471;
-#[allow(dead_code)]
-pub const JAR_JAR_COLOR_HEX: u32 = 0xD59D7E;
-#[allow(dead_code)]
-pub const HORRIFIED_JAR_JAR_PIC: &str =
-    "https://cdn.mos.cms.futurecdn.net/RvLDChLaR37NWTEjvQm2pB-970-80.jpg.webp";
-#[allow(dead_code)]
-pub const HAPPY_JAR_JAR_PIC: &str =
-    "https://static.wikia.nocookie.net/unanything/images/c/c7/Jar_Jar.jpg/revision/latest";
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    nicknamer: NicknamerConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RevealConfig {
+    pub insult: String,
+    pub role_to_mention: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NicknamerConfig {
+    reveal: RevealConfig,
+}
+
+impl Config {
+    fn new() -> anyhow::Result<Self> {
+        let s = config::Config::builder()
+            .add_source(config::File::with_name("nicknamer/config"))
+            .build()?;
+
+        Ok(s.try_deserialize()?)
+    }
+}
