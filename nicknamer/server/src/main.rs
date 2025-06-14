093be974f9523882664b6b87197987ccfb06159c
tacascer <trandangtrithanh2000@gmail.com>
2025-06-14 18:07:36 +0100

    feat: add configuration loading from .env file using envy

--- nicknamer/server/src/main.rs
+++ nicknamer/server/src/main.rs
@@ -1,4 +1,28 @@
+use tracing::info;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
+    let cfg = config::Config::new();
+    info!("Configuration loaded: {:?}", cfg);
+    Ok(())
+}
+
+mod config {
+    use dotenvy::dotenv_iter;
+    use serde::Deserialize;
+
+    #[derive(Deserialize, Debug)]
+    pub struct Config {
+        pub db_url: String,
+    }
+
+    impl Config {
+        pub fn new() -> Self {
+            let iter = dotenv_iter()
+                .expect("Failed to load .env file")
+                .map(|res| res.expect("Failed to read environment variable"));
+            envy::from_iter(iter).expect("Failed to parse environment variables into Config")
+        }
+    }
 }
