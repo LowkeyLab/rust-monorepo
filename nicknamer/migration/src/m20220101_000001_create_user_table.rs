53e4e2373b1624f148ef9103034ae067e15daeab
tacascer <trandangtrithanh2000@gmail.com>
2025-06-14 17:45:35 +0100

    feat: add user table migration with creation and rollback logic

--- nicknamer/migration/src/m20220101_000001_create_user_table.rs
+++ nicknamer/migration/src/m20220101_000001_create_user_table.rs
@@ -6,36 +6,35 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(pk_auto(User::Id))
+                    .col(
+                        ColumnDef::new(User::DiscordId)
+                            .big_unsigned()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(ColumnDef::new(User::Name).string().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(User::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum User {
     Table,
     Id,
-    Title,
-    Text,
+    DiscordId,
+    Name,
 }
