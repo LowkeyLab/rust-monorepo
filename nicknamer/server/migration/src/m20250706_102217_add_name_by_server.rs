6e86faaf622a430d28e1573ceafd2be16d5526d1
tacascer <trandangtrithanh2000@gmail.com>
2025-07-15 18:55:10 +0100

    feat: add migration

---
+++ nicknamer/server/migration/src/m20250706_102217_add_name_by_server.rs
@@ -0,0 +1,34 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.alter_table(Table::alter()
+            .table(Name::Table)
+            .add_column(
+                ColumnDef::new(Name::ServerId)
+                    .string()
+                    .not_null()
+                    .default("89467777677468954757"),
+            )
+            .to_owned())
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.alter_table(Table::alter()
+            .table(Name::Table)
+            .drop_column(Name::ServerId)
+            .to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Name {
+    Table,
+    ServerId,
+}
