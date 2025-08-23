22f5c8e1452652f4cbbf693f9887f73dbae4a705
tacascer <trandangtrithanh2000@gmail.com>
2025-08-23 17:08:07 +0100

    feat: add mindreadr_web_entities package with initial dependencies and update workspace configuration

--- mindreadr/web/migration/src/m20250822_000001_create_games_table.rs
+++ mindreadr/web/migration/src/m20250822_000001_create_games_table.rs
@@ -1,12 +1,13 @@
-use crate::sea_orm::{EnumIter, Iterable};
+use sea_orm::{EnumIter, Iterable};
+use sea_orm_migration::prelude::extension::postgres::Type;
 use sea_orm_migration::prelude::*;
-use sea_orm_migration::schema::{enumeration, json, pk_auto, string, timestamp_with_time_zone};
+use sea_orm_migration::schema::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[derive(DeriveIden)]
-enum Game {
+enum Games {
     Table,
     Id,
     Players,
@@ -16,41 +17,142 @@ enum Game {
     UpdatedAt,
 }
 
-#[derive(Iden, EnumIter)]
-pub enum GameState {
-    #[iden = "waiting_for_players"]
+#[derive(DeriveIden)]
+struct GameState;
+
+#[derive(DeriveIden, EnumIter)]
+pub enum GameStateEnum {
     WaitingForPlayers,
-    #[iden = "in_progress"]
     InProgress,
-    #[iden = "finished"]
     Finished,
 }
+
+#[derive(DeriveIden)]
+enum Rounds {
+    Table,
+    GameId,
+    Number,
+}
+
+#[derive(DeriveIden)]
+enum RoundGuesses {
+    Table,
+    GameId,
+    RoundNumber,
+    PlayerId,
+    Guess,
+}
+
+const FK_ROUNDS_TO_GAMES: &str = "fk-rounds-game_id";
+const ROUNDS_UNIQUE_CONSTRAINT: &str = "idx-rounds-game_id-number";
+const ROUND_GUESSES_UNIQUE_CONSTRAINT: &str = "idx-round_guesses-game_id-round_number-player_id";
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
+            .create_type(
+                Type::create()
+                    .as_enum(GameState)
+                    .values(GameStateEnum::iter())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
             .create_table(
                 Table::create()
-                    .table(Game::Table)
+                    .table(Games::Table)
                     .if_not_exists()
-                    .col(pk_auto(Game::Id))
-                    .col(json(Game::Players))
-                    .col(string(Game::Name))
+                    .col(pk_auto(Games::Id))
+                    .col(json(Games::Players))
+                    .col(string(Games::Name))
                     .col(enumeration(
-                        Game::State,
-                        Alias::new("state"),
-                        GameState::iter(),
+                        Games::State,
+                        Alias::new("game_state"),
+                        GameStateEnum::iter(),
                     ))
                     .col(
-                        timestamp_with_time_zone(Game::CreatedAt)
+                        timestamp_with_time_zone(Games::CreatedAt)
                             .default(Expr::current_timestamp()),
                     )
                     .col(
-                        timestamp_with_time_zone(Game::UpdatedAt)
-                            .default(Expr::current_timestamp())
-                            .extra("ON UPDATE CURRENT_TIMESTAMP".to_string()),
+                        timestamp_with_time_zone(Games::UpdatedAt)
+                            .default(Expr::current_timestamp()),
                     )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Rounds::Table)
+                    .if_not_exists()
+                    .col(integer(Rounds::GameId))
+                    .col(integer(Rounds::Number))
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(RoundGuesses::Table)
+                    .if_not_exists()
+                    .col(integer(RoundGuesses::GameId))
+                    .col(integer(RoundGuesses::RoundNumber))
+                    .col(integer(RoundGuesses::PlayerId))
+                    .col(string(RoundGuesses::Guess))
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_foreign_key(
+                ForeignKey::create()
+                    .name(FK_ROUNDS_TO_GAMES)
+                    .from(Rounds::Table, Rounds::GameId)
+                    .to(Games::Table, Games::Id)
+                    .on_delete(ForeignKeyAction::Cascade)
+                    .on_update(ForeignKeyAction::NoAction)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .name(ROUNDS_UNIQUE_CONSTRAINT)
+                    .table(Rounds::Table)
+                    .col(Rounds::GameId)
+                    .col(Rounds::Number)
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .name(ROUND_GUESSES_UNIQUE_CONSTRAINT)
+                    .table(RoundGuesses::Table)
+                    .col(RoundGuesses::GameId)
+                    .col(RoundGuesses::RoundNumber)
+                    .col(RoundGuesses::PlayerId)
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_foreign_key(
+                ForeignKey::create()
+                    .name("fk-round_guesses-game_id")
+                    .from(RoundGuesses::Table, RoundGuesses::GameId)
+                    .to(Games::Table, Games::Id)
+                    .on_delete(ForeignKeyAction::Cascade)
+                    .on_update(ForeignKeyAction::NoAction)
                     .to_owned(),
             )
             .await
@@ -58,7 +160,26 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Game::Table).if_exists().to_owned())
+            .drop_table(Table::drop().table(Games::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Rounds::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(RoundGuesses::Table).to_owned())
+            .await?;
+        manager
+            .drop_index(Index::drop().name(ROUNDS_UNIQUE_CONSTRAINT).to_owned())
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(ROUND_GUESSES_UNIQUE_CONSTRAINT)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_foreign_key(ForeignKey::drop().name(FK_ROUNDS_TO_GAMES).to_owned())
             .await
     }
 }
