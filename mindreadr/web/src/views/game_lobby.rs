890d0aabd3703e32428e5f8cf4226d6b1839be57
tacascer <trandangtrithanh2000@gmail.com>
2025-08-24 19:21:30 +0100

    feat: remove unused CSS classes to streamline styling and improve maintainability

--- mindreadr/web/src/views/game_lobby.rs
+++
@@ -1,5 +0,0 @@
-//! Deprecated: GameLobby moved to `views::games::lobby`.
-//! This file is retained temporarily to avoid confusion and can be deleted.
-
-// Re-export (only if this module were re-enabled) - not used because `mod game_lobby;` was removed.
-// pub use crate::views::games::GameLobby;
